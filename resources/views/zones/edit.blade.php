746b51abe2fba7a1489896b33cfca89b42039a62
abdulkharim6435 <148478453+abdulkharim6435@users.noreply.github.com>
2025-09-15 15:01:27 +0700

    zona edit delete part 4

--- resources/views/zones/edit.blade.php
+++ resources/views/zones/edit.blade.php
@@ -31,17 +31,28 @@
     <!-- Zona -->
     <div class="mb-4">
         <label class="block mb-1 font-semibold">Nama Zona</label>
-        <input type="text" name="zona" value="{{ old('zona', $zone->zona) }}" class="w-full border px-3 py-2 rounded" required>
+        <input type="text" name="zona" value="{{ old('zona', $zone->zona) }}" class="w-full border px-3 py-2 rounded" disabled>
         @error('zona')
             <span class="text-red-500 text-sm">{{ $message }}</span>
         @enderror
     </div>
 
     <!-- Tombol Submit -->
-    <div class="flex justify-end">
+    <!-- <div class="flex justify-end">
         <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
             Simpan Perubahan
         </button>
+    </div> -->
+    <div class="flex gap-2">
+        <button type="submit"
+            class="flex-1 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 text-center">
+            Simpan
+        </button>
+
+        <a href="{{ route('zones') }}"
+        class="flex-1 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 text-center">
+        Batal
+        </a>
     </div>
 </form>
 @endsection
